c5c77be8769c0e76e5a0349656c9c40146be3053
Michael Garvin <gar@andyet.net>
2013-10-14 19:17:40 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+var Moonboots = require('moonboots');
+function routeConfig(contentType, clientApp) {
+  var config = {
+    payload: {
+      override: contentType
+    }
+  };
+
+  if (!clientApp.getConfig('developmentMode')) {
+    config.cache = {
+      expiresIn: clientApp.getConfig('cachePeriod')
+    };
+  }
+  console.log(config);
+  return config;
+}
+
+function jsHandler (clientApp) {
+  return function(request) {
+    clientApp.sourceCode(function _getSourceCode(source) {
+      request.reply(source);
+    });
+  };
+}
+
+function cssHandler (clientApp) {
+  return function (request) {
+    request.reply(clientApp.css());
+  };
+}
+
+function mainHandler(clientApp) {
+  return function (request) {
+    clientApp.getResult('html', function(html) {
+      request.reply(html);
+    });
+  };
+}
+
+module.exports = {
+    register: function(server, options, next) {
+      var clientApp = new Moonboots(options);
+      server.route({
+        method: 'get',
+        path: '/' + encodeURIComponent(clientApp.jsFileName()) + '*.js',
+        handler: jsHandler(clientApp),
+        config: routeConfig('text/javascript; charset=utf-8', clientApp)
+      });
+      server.route({
+        method: 'get',
+        path: '/' + encodeURIComponent(clientApp.jsFileName()) + '*.css',
+        handler: cssHandler(clientApp),
+        config: routeConfig('text/css; charset=utf-8', clientApp)
+      });
+      server.route({
+        method: 'get',
+        path: '/{client*}',
+        handler: mainHandler(clientApp)
+      });
+      next();
+    }
+};
