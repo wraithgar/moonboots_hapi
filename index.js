be5fa192a02c1ee119bcd14b2afe47977e6d5cf0
Henrik Joreteg <hjoreteg@gmail.com>
2014-02-25 16:46:20 -0800

    explicitly set cache-control to no-store on default app route handler

--- index.js
+++ index.js
@@ -80,7 +80,10 @@ exports.register = function (plugin, options, next) {
                     if (err) {
                         return reply(new HapiError.internal('No html result'));
                     }
-                    reply(html);
+                    // Set cache-control explicitly to no-store
+                    // this is so chrome doesn't trust it's cache
+                    // even on back button presses.
+                    reply(html).header('cache-control', 'no-store');
                 });
             };
         }
